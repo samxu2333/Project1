fa1c9de1d764f7dd0417049e1f7ae1577a5ad30a
DGM6308 <DGM6308@labB2QOI1>
2024-01-23 21:27:16 +0000

    Add project files.

---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void heightInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        private void heightInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(heightInput.Text, out int rectHeight))
+            {
+                {
+                    rectangle1.Height = rectHeight;
+                }              
+            }
+        }
+    }
+}
